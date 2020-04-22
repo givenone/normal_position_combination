3d257535ebcdba990a26b3c39622530f95ec77e4
givenone <givenone1@gmail.com>
2020-04-22 15:59:50 +0900

    point cloud done

--- generate_pointcloud.py
+++ generate_pointcloud.py
@@ -43,12 +43,19 @@ colored 3D point cloud in the PLY format.
 import argparse
 import sys
 import os
+import cv2 as cv
 from PIL import Image
-
-focalLength = 525.0
-centerX = 319.5
-centerY = 239.5
-scalingFactor = 5000.0
+from numpy import array
+focalLength = 0.05
+width = 2160
+height = 3840
+camera = -4.9 # z axis
+sensor_width = 0.025
+sensor_height = 0.024
+centerX = width / 2
+centerY = height / 2
+x_pitch = sensor_width / width
+y_pitch = sensor_height / height
 
 def generate_pointcloud(rgb_file, depth_file, normal_file, ply_file):
     """
@@ -61,30 +68,36 @@ def generate_pointcloud(rgb_file, depth_file, normal_file, ply_file):
     ply_file -- filename of ply file
     
     """
-    rgb = Image.open(rgb_file)
-    depth = Image.open(depth_file)
-    normal = Image.open(normal_file)
+    rgb = cv.imread(rgb_file, -1)
+    rgb = cv.cvtColor(rgb, cv.COLOR_BGR2RGB)
+    depth = cv.imread(depth_file, -1)
+    depth = cv.cvtColor(depth, cv.COLOR_BGR2RGB)
+    normal = cv.imread(normal_file, -1)
+    normal = cv.cvtColor(normal, cv.COLOR_BGR2RGB)
+
+    #normal = Image.open(normal_file)
     
     if rgb.size != depth.size:
         raise Exception("Color and depth image do not have the same resolution.")
     if depth.size != normal.size :
         raise Exception("Depth image and normal map do not have the same resolution.")
-    if rgb.mode != "RGB":
-        raise Exception("Color image is not in RGB format")
-    if depth.mode != "I":
-        raise Exception("Depth image is not in intensity format")
-
-
-    points = []    
-    for v in range(rgb.size[1]):
-        for u in range(rgb.size[0]):
-            color = rgb.getpixel((u,v))
-            N = normal.getpixel((u,v))
-            Z = depth.getpixel((u,v)) / scalingFactor
+    points = []
+    range_grid = []    
+    for v in range(width):
+        for u in range(height):
+            color = rgb[u][v]
+            N = normal[u][v]
+            Z = depth[u][v][0]
             if Z==0: continue
-            X = (u - centerX) * Z / focalLength
-            Y = (v - centerY) * Z / focalLength
-            points.append("%f %f %f %f %f %f %d %d %d 0\n"%(X,Y,Z, N[0], N[1], N[2], color[0],color[1],color[2]))
+            X = (u - centerX) * Z / focalLength * x_pitch
+            Y = (centerY - v) * Z / focalLength * y_pitch
+            Z = Z #+ camera
+            
+            if abs(Z-4.9) > 3 :
+                range_grid.append(False) 
+                continue
+            points.append("%f %f %f %f %f %f %d %d %d\n"%(X,Y,Z, N[0], N[1], N[2], color[0],color[1],color[2])) # BGR -> RGB
+            range_grid.append(True)
     file = open(ply_file,"w")
     file.write('''ply
 format ascii 1.0
@@ -98,19 +111,27 @@ property float nz
 property uchar red
 property uchar green
 property uchar blue
+element range_grid %d
+property list uchar int vertex_indices
 end_header
 %s
-'''%(len(points),"".join(points)))
+'''%(len(points),len(range_grid),"".join(points)))
+    for i, flag in enumerate(range_grid) :
+        if flag :
+            file.write("%d %d\n"%(1, i))
+        else :
+            file.write("%d %d\n"%(0, i))
     file.close()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''
     This script reads a registered pair of color and depth images and generates a colored 3D point cloud in the
     PLY format. 
     ''')
-    parser.add_argument('rgb_file', help='input color image (format: png)')
-    parser.add_argument('depth_file', help='input depth image (format: png)')
-    parser.add_argument('normal_file', help='input depth image (format: png)')
+    parser.add_argument('rgb_file', help='input color image (format: tiff)')
+    parser.add_argument('depth_file', help='input depth image (format: tiff)')
+    parser.add_argument('normal_file', help='input depth image (format: tiff)')
     parser.add_argument('ply_file', help='output PLY file (format: ply)')
     args = parser.parse_args()
 
