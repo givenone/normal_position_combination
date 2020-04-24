68deebb2a9ab2388a76e352de802561502af3c23
givenone <givenone1@gmail.com>
2020-04-24 18:04:39 +0900

    testing

--- split.sh
+++ split.sh
@@ -7,9 +7,9 @@ for i in $SET
         for j in $SET
         do
         echo "Running loop seq "$i $j
-        ./mesh_opt "before_correction/output$i $j.ply" -lambda 0.8 "norm:after_correction_optimized/result$i $j.ply"
+        ./mesh_opt "before_correction/output$i $j.ply" "norm:after_correction_optimized/result$i $j.ply"
         # lambda : default 0.1, weight for corrected normal.
     done
 done
 
-python mesh_merge.py after_correction/result final_binary_optimized_0.8.ply
+python mesh_merge.py after_correction/result final_binary_optimized.ply
